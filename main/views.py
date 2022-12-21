83f8ba4c607deee8443506c6ef4fc82632dd11e2
Cola <khoronenko24@gmail.com>
2022-12-21 11:28:03 +0200

    second commit

--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница!',
+        'values': ['Some', 'Hello', '123']
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html') 
